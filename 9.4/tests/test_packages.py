4b3415cfe78175dccfcc33501a7e82178ec2008a
Jean Valverde <moimog33@gmail.com>
2023-06-26 10:48:47 +0100

    chore: update node 18

--- 9.4/tests/test_packages.py
+++ 9.4/tests/test_packages.py
@@ -18,3 +18,6 @@ def test_packages_installed(host, name):
   ''' Test a minimum of required packages '''
   pkg = host.package(name)
   assert pkg.is_installed
+
+def test_node(host):
+  assert host.check_output('node -v').startswith('v18')
