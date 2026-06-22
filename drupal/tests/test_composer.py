d84eb27d7439a60b08834ca28e218c50d3e3072c
Jean Valverde <moimog33@gmail.com>
2026-06-22 12:29:09 +0200

    fix: guzzle update workaround

--- drupal/tests/test_composer.py
+++ drupal/tests/test_composer.py
@@ -4,22 +4,22 @@ def test_composer(host):
   assert host.check_output('composer -V').startswith('Composer version 2')
   assert host.file("/var/www/.composer/composer.json").exists
 
-@pytest.mark.parametrize("bin", [
-  ("parallel-lint"),
-  ("pdepend"),
-  ("phpcs"),
-  ("phploc"),
-  ("phpmd"),
-  ("phpmetrics"),
-  ("phpqa"),
-  ("phpstan"),
-  ("robo"),
+@pytest.mark.parametrize("bin,version", [
+  ("parallel-lint", "PHP Parallel Lint"),
+  ("pdepend", ""),
+  ("phpcs", "PHP_CodeSniffer version"),
+  ("phploc", "phploc"),
+  ("phpmd", ""),
+  ("phpmetrics", "\nPhpMetrics"),
+  ("phpqa", "Robo"),
+  ("phpstan", "PHPStan"),
+  ("robo", "Robo"),
 ])
-def test_composer_bin(host, bin):
+def test_composer_bin(host, bin, version):
   ''' Test bin exist '''
   assert host.file("/var/www/.composer/vendor/bin/" + bin).exists
   assert host.file("/usr/local/bin/" + bin).is_symlink
-  assert host.file("/usr/local/bin/" + bin).mode == 0o777
+  assert host.check_output("/usr/local/bin/" + bin + " --version").startswith(version)
 
 def test_phpcs_standard(host):
   '''' Test phpcs standards '''
